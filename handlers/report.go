afcadcffa92ff415bd24e4be91ce226bfb7b1f71
Vladislav Shub <vlad@yotpo.com>
2016-03-28 14:46:19 +0300

    Clean version without google analytics and without custom domain option

--- handlers/report.go
+++ handlers/report.go
@@ -5,12 +5,8 @@ import (
 	"log"
 	"net/http"
 
-	"flag"
 	"html/template"
 )
-
-var domain = flag.String("domain", "", "HTTP listen address")
-var googleAnalyticsKey = flag.String("google_analytics_key", "UA-58936835-1", "Google Analytics Account Id")
 
 // ReportHandler handles the report page
 func ReportHandler(w http.ResponseWriter, r *http.Request, repo string) {
@@ -34,7 +30,5 @@ func ReportHandler(w http.ResponseWriter, r *http.Request, repo string) {
 		"repo":                 repo,
 		"response":             string(respBytes),
 		"loading":              needToLoad,
-		"domain":               domain,
-		"google_analytics_key": googleAnalyticsKey,
 	})
 }
